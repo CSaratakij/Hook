5fccad9643d492df569136f2483e03985087fe81
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-28 14:28:14 +0700

    Added ability to the box to be able to move when player push.

---
+++ Hook/box/box.gd
@@ -0,0 +1,42 @@
+
+extends KinematicBody2D
+
+const GRAVITY = 300.0
+const PUSH_SPEED = 80.0
+
+var velocity = Vector2()
+var motion = Vector2()
+var move_dir = Vector2()
+
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	velocity.y += GRAVITY * delta
+	velocity.x = move_dir.x * PUSH_SPEED
+
+	motion = velocity * delta
+	move(motion)
+	
+	if is_colliding():
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
+
+func _on_left_body_enter( body ):
+	if body.get_groups().has("player"):
+		move_dir.x = 1.0
+
+func _on_left_body_exit( body ):
+	if body.get_groups().has("player"):
+		move_dir.x = 0.0
+
+func _on_right_body_enter( body ):
+	if body.get_groups().has("player"):
+		move_dir.x = -1.0
+
+func _on_right_body_exit( body ):
+	if body.get_groups().has("player"):
+		move_dir.x = 0.0
