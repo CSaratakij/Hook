99e0c174f3a824932c4b4c6221af697e265693b2
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-28 16:46:43 +0700

    Changed game menu process from process loop (frame) to input process loop (input event).

--- Hook/menu/game_menu.gd
+++ Hook/menu/game_menu.gd
@@ -1,19 +1,38 @@
 
 extends Panel
 
+const MAINMENU_PATH = "res://menu/mainmenu.tscn"
+
 onready var tree = get_tree()
-onready var player_camera = tree.get_nodes_in_group("player_camera")
+onready var player_cameras = tree.get_nodes_in_group("player_camera")
 
 func _ready():
 	hide()
 	set_process(true)
+	set_process_input(true)
 
-func _process(delta):
-	if tree.is_paused():
-		if not player_camera.empty():
+func _input(event):
+	if event.is_action("quit") and event.is_pressed() and not event.is_echo():
+		tree.set_pause(!tree.is_paused())
+		if is_hidden() and not player_cameras.empty():
 			var half_rect_size = get_size() / 2
-			var offset = player_camera[0].get_camera_screen_center() - half_rect_size
+			var offset = player_cameras[0].get_camera_screen_center() - half_rect_size
 			set_global_pos(offset)
 			show()
-	else:
-		hide()
+		else:
+			hide()
+
+func _on_btnResume_pressed():
+	hide()
+	tree.set_pause(false)
+
+func _on_btnRestart_pressed():
+	tree.reload_current_scene()
+	tree.set_pause(false)
+
+func _on_btnBackToMainMenu_pressed():
+	tree.set_pause(false)
+	tree.change_scene(MAINMENU_PATH)
+
+func _on_btnQuit_pressed():
+	tree.quit()
--- Hook/menu/mainmenu.gd
+++ Hook/menu/mainmenu.gd
@@ -4,5 +4,12 @@ extends Panel
 const NEXT_SCENE = "res://level/level.tscn"
 onready var tree = get_tree()
 
+func _ready():
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("play"):
+		tree.change_scene(NEXT_SCENE)
+
 func _on_btnPlay_pressed():
 	tree.change_scene(NEXT_SCENE)
