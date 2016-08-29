ab58bb73311cba5af246664efba365f8de756b27
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-30 01:56:34 +0700

    Fixed change level logic bugs and successful adding 5 level :)

--- Hook/scripts/global.gd
+++ Hook/scripts/global.gd
@@ -6,7 +6,10 @@ const MAINMENU = "res://menu/mainmenu.tscn"
 const THANK_YOU_FOR_PLAYING = "res://menu/gameover.tscn"
 const LEVELS = [
 	"res://level/level.tscn",
-	"res://level/level1.tscn"
+	"res://level/level1.tscn",
+	"res://level/level2.tscn",
+	"res://level/level3.tscn",
+	"res://level/level4.tscn"
 	]
 
 onready var tree = get_tree()
@@ -22,6 +25,6 @@ func change_level(level_id):
 	
 func change_to_next_level(current):
 	if current < LEVELS.size() -1:
-		tree.change_scene(LEVELS[current - 1])
+		tree.change_scene(LEVELS[current + 1])
 	else:
 		tree.change_scene(THANK_YOU_FOR_PLAYING)
