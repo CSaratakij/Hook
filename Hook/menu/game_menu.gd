69467913361920cf2fa69586af900cd0ea309eac
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-29 18:18:22 +0700

    Added sound to the select menu (when it appear and disapear)

--- Hook/menu/game_menu.gd
+++ Hook/menu/game_menu.gd
@@ -5,6 +5,7 @@ onready var tree = get_tree()
 onready var root = tree.get_root()
 onready var global = root.get_node("/root/scripts/global")
 onready var player_cameras = tree.get_nodes_in_group("player_camera")
+onready var _audio_player = get_node("SamplePlayer")
 
 func _ready():
 	hide()
@@ -14,6 +15,7 @@ func _ready():
 func _input(event):
 	if event.is_action("quit") and event.is_pressed() and not event.is_echo():
 		tree.set_pause(!tree.is_paused())
+		_audio_player.play("select")
 		if is_hidden() and not player_cameras.empty():
 			var half_rect_size = get_size() / 2
 			var offset = player_cameras[0].get_camera_screen_center() - half_rect_size
